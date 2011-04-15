3f87941edb1034234f2db85812b32e3d747ff039
Martin Stensgård <mastensg@ping.uio.no>
2011-04-15 18:10:22 +0200

    firmware/main: add lcd functionality

--- firmware/main.c
+++ firmware/main.c
@@ -3,6 +3,7 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+#include <util/twi.h>
 
 #define FRAMERATEADDR   9
 #define FRAMERATEDFLT   16
@@ -24,6 +25,24 @@
 #define CAMERA_PIN      PINA
 #define CAMERA_PORT     PORTA
 
+#define LCD_DISPLAY_ON      0x41
+#define LCD_DISPLAY_OFF     0x42
+#define LCD_CURSOR_SET      0x45
+#define LCD_CURSOR_HOME     0x46
+#define LCD_CURSOR_ON       0x47
+#define LCD_CURSOR_OFF      0x48
+#define LCD_CURSOR_LEFT     0x49
+#define LCD_CURSOR_RIGHT    0x4a
+#define LCD_BLINK_ON        0x4b
+#define LCD_BLINK_OFF       0x4c
+#define LCD_BACKSPACE       0x4e
+#define LCD_CLEAR_SCREEN    0x51
+#define LCD_CONTRAST_SET    0x52
+#define LCD_BRIGHTNESS_SET  0x53
+#define LCD_LOAD_CUSTOM     0x54
+#define LCD_SCROLL_LEFT     0x55
+#define LCD_SCROLL_RIGHT    0x56
+
 #define LED_DDR         DDRD
 #define LED_PIN         PIND
 #define LED_PORT        PORTD
@@ -37,10 +56,13 @@
 #define TRIGGER_IN      (1 << PD2)
 #define TRIGGER_OUT     (1 << PD5)
 
+#define TWI_SLA_DISP    0x50
+
 static volatile uint8_t multipliers[4] = {0, 0, 0, 0};
 static volatile uint8_t time = 0;
 static volatile uint8_t cameras = 0b10101010;
 static volatile uint16_t framerate = 16;
+static volatile uint8_t cursor = 0x40;
 
 ISR(INT0_vect) {
     CAMERA_PORT = cameras;
@@ -147,46 +169,235 @@ trigger_output_init(uint16_t framerate) {
 }
 
 static void
+twi_send_byte(uint8_t byte) {
+start:
+    // Send start condition.
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+    while(!(TWCR & (1 << TWINT)));
+    switch(TW_STATUS) {
+    case TW_REP_START:
+    case TW_START:
+        break;
+
+    default:
+        return;
+    }
+
+    // Send SLA+W.
+    TWDR = TWI_SLA_DISP | TW_WRITE;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while(!(TWCR & (1 << TWINT)));
+    switch(TW_STATUS) {
+    case TW_MT_SLA_ACK:
+        break;
+
+    case TW_MT_SLA_NACK:
+    case TW_MT_ARB_LOST:
+        goto start;
+
+    default:
+        goto end;
+    }
+
+    // Send character.
+    TWDR = byte;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while(!(TWCR & (1 << TWINT)));
+
+end:
+    // Send stop condition.
+    TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
+}
+
+static void
+lcd_command(uint8_t command) {
+    twi_send_byte(0xfe);
+    twi_send_byte(command);
+}
+
+static void
+lcd_putchar(uint8_t c) {
+    /*
+    static uint8_t col = 0;
+
+    if(c == '\n') {
+        for(; col < 40; ++col)
+            twi_send_byte(' ');
+
+        col = 0;
+        return;
+    }
+
+    if(col == 16) {
+        for(; col < 40; ++col)
+            twi_send_byte(' ');
+
+        col = 0;
+    }
+    */
+
+    twi_send_byte(c);
+    //++col;
+}
+
+static void
+lcd_puts(char *s) {
+    uint8_t i = 0;
+    while(s[i]) {
+        if(s[i] == '\n') {
+            lcd_command(LCD_CURSOR_SET);
+            lcd_putchar(0x40);
+        } else {
+            lcd_putchar(s[i]);
+        }
+
+        ++i;
+    }
+}
+
+static void
+lcd_putdigit(uint8_t n) {
+    lcd_putchar('0' + n);
+}
+
+static void
+lcd_putuint(uint16_t n) {
+    if(n >= 1000)
+        lcd_putdigit(n / 1000);
+
+    if(n >= 100)
+        lcd_putdigit((n % 1000) / 100);
+
+    if(n >= 10)
+        lcd_putdigit((n % 100) / 10);
+
+    lcd_putdigit(n % 10);
+}
+
+static void
+lcd_message(char *msg) {
+    lcd_command(LCD_CLEAR_SCREEN);
+    lcd_command(LCD_CURSOR_HOME);
+    lcd_puts(msg);
+}
+
+static void
+lcd_init(void) {
+    // Set bitrate.
+    TWBR = 77; // XXX: Make more dynamic.
+
+    lcd_command(LCD_DISPLAY_ON);
+    lcd_command(LCD_BLINK_ON);
+
+    lcd_message("Mini trigger box\n   Simula  RL   ");
+}
+
+static void
+lcd_update(void) {
+    lcd_command(LCD_CLEAR_SCREEN);
+
+    for(uint8_t i = 0; i < 4; ++i) {
+        lcd_command(LCD_CURSOR_SET);
+        lcd_putchar(4 * i);
+        lcd_putdigit(1 << multipliers[i]);
+        lcd_putchar('x');
+        lcd_putchar('D');
+    }
+
+    lcd_command(LCD_CURSOR_SET);
+    lcd_putchar(0x40);
+    lcd_putuint(framerate);
+    lcd_puts("hz");
+
+    lcd_command(LCD_CURSOR_SET);
+    twi_send_byte(0x40 + 4 * 2);
+    lcd_puts("sto");
+    lcd_puts(" menu");
+
+    lcd_command(LCD_CURSOR_SET);
+    twi_send_byte(cursor);
+}
+
+static void
 button_handle_release(uint8_t button) {
 }
 
 static void
 button_handle_press(uint8_t button) {
-    switch(button) {
-    case BUTTON_1:
-        if(framerate <= 500)
-            OCR1A = PERIOD(++framerate);
+    if(button == BUTTON_2) {
+        switch(cursor) {
+        case 12:
+            cursor = 0x40;
+            break;
+        case 0x40:
+            cursor = 0x40 + 8;
+            break;
+        case 0x40 + 12:
+            cursor = 0;
+            break;
+        default:
+            cursor += 4;
+            break;
+        }
+
+        lcd_update();
+        return;
+    }
+
+    uint8_t m;
+
+    switch(cursor) {
+    case 0x40:
+        // Set framerate.
+        if(button == BUTTON_1) {
+            if(framerate <= 500)
+                OCR1A = PERIOD(++framerate);
+        } else {
+            if(framerate > 2)
+                OCR1A = PERIOD(--framerate);
+        }
         break;
-    case BUTTON_2:
-        // Save framerate.
-        LED_PORT |= LED_RED;
+    case 0x40 + 8:
+        // Store settings.
         eeprom_busy_wait();
         eeprom_write_byte((uint8_t *)FRAMERATEADDR, framerate);
-        LED_PORT &= ~LED_RED;
+        lcd_message("Saved!");
         break;
-    case BUTTON_3:
-        if(framerate > 2)
-            OCR1A = PERIOD(--framerate);
+    case 0x40 + 12:
+        // Enter menu.
         break;
     default:
+        // Set multiplier.
+        m = cursor / 4;
+
+        if(button == BUTTON_1) {
+            if(multipliers[m] < 3)
+                ++multipliers[m];
+        } else {
+            if(multipliers[m] > 0)
+                --multipliers[m];
+        }
+
         break;
     }
+
+    lcd_update();
 }
 
 static void
 button_handle_hold(uint8_t button) {
-    switch(button) {
-    case BUTTON_1:
-        if(framerate <= 500)
-            OCR1A = PERIOD(++framerate);
-        break;
-    case BUTTON_3:
-        if(framerate > 2)
-            OCR1A = PERIOD(--framerate);
-        break;
-    default:
-        break;
+    if(cursor == 0x40) {
+        // Set framerate.
+        if(button == BUTTON_1) {
+            if(framerate <= 500)
+                OCR1A = PERIOD(++framerate);
+        } else {
+            if(framerate > 2)
+                OCR1A = PERIOD(--framerate);
+        }
     }
+
+    lcd_update();
 }
 
 int
@@ -194,6 +405,8 @@ main(void) {
     uint8_t button_jiffies[3] = {0, 0, 0};
     uint8_t button_held = 0b00000000;
     uint8_t button_state = 0b00000000;
+
+    lcd_init();
 
     eeprom_busy_wait();
     framerate = eeprom_read_byte((const uint8_t *)FRAMERATEADDR);
@@ -222,6 +435,8 @@ main(void) {
     LED_PORT ^= LED_RED;
 
     sei();
+
+    lcd_update();
 
     for (;;) {
         _delay_ms(10);
